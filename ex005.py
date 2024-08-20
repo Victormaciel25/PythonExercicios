bf7dd61b9378dc717ad9c4c258a1fd9bd53a53f8
Victor Maciel <victormaciel.dev@gmail.com>
2024-08-20 14:42:18 -0300

    up

---
+++ ex005.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print(f'Analisando o valor {n}, seu antecessor é {a} e o sucessor é {s}.')
+
+# ou
+
+n = int(input('Digite um número: '))
+print(f'Analisando o valor {n}, seu antecessor é {n - 1} e o sucessor é {n + 1}.')
---
+++ ex006.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} valor {n * 2}.')
+print(f'O triplo de {n} valor {n * 3}.')
+print(f'A raiz quadrada de {n} é igual a {n ** 0.5}.')
