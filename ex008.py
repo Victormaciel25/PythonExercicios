06f5ba7fd23a309249564ee2ad4da6a94c42a658
Victor Maciel <victormaciel.dev@gmail.com>
2024-08-21 12:16:45 -0300

    up

---
+++ ex008.py
@@ -0,0 +1,8 @@
+m = float(input('Uma distância em metros: '))
+dam = m / 10
+hm = m / 100
+km = m / 1000
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+print(f'A medida de {m}m corresponde a \n{dam}dam\n{hm}hm\n{km}km\n{dm}dm\n{cm}cm\n{mm}mm')
