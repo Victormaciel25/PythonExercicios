3439c518fb65d2b7fb6c0ad64512cf7e45a27c53
Victor Maciel <victormaciel.dev@gmail.com>
2024-08-24 00:37:55 -0300

    up

--- ex019.py
+++ ex019.py
@@ -1,11 +1,4 @@
 import random
-
-nome1 = input('Primeiro aluno: ')
-nome2 = input('Segundo aluno: ')
-nome3 = input('Terceiro aluno: ')
-nome4 = input('Quarto aluno: ')
-
-opcoes = [nome1, nome2, nome3, nome4]
+opcoes = [input(f'Aluno {i+1}: ') for i in range(4)]
 sorteado = random.choice(opcoes)
-
 print(f'O aluno escolhido foi {sorteado}')
