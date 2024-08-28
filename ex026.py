c1e4941f00ab2d2b783c5ad7d9e415da9971aaaa
Victor Maciel <victormaciel.dev@gmail.com>
2024-08-28 19:01:19 -0300

    Update ex026.py

--- ex026.py
+++ ex026.py
@@ -1,3 +1,16 @@
-frase = input('Digite uma frase: ')
-quantidade_a = frase.lower().count('a')
+frase = input('Digite uma frase: ').upper().strip()
+quantidade_a = frase.count('A')
+posicao_a = frase.find('A')
+ultima_posicao_a = frase.rfind('A')
+
 print(f'A letra "A" aparece {quantidade_a} vezes na frase.')
+
+if posicao_a != -1:
+    print(f"A primeira letra 'A' aparece na posição {posicao_a + 1}.")
+else:
+    print("A letra primeira letra 'A' não foi encontrada na frase.")
+
+if ultima_posicao_a != -1:
+    print(f"A última letra 'A' aparece na posição {ultima_posicao_a + 1}.")
+else:
+    print("A letra ultima letra 'A' não foi encontrada na frase.")
