44e15f8d787885c65c3fd84b1dcb2ef60b6c1052
Victor Maciel <victormaciel.dev@gmail.com>
2024-10-02 22:26:32 -0300

    .

---
+++ ex042.py
@@ -0,0 +1,10 @@
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+
+if r1 == r2 and r1 == r3 :
+    print('Os segmentos acima PODEM FORMAR triângulo EQUILÁTERO!')
+elif r1 == r2 or r1 == r3 or r2 == r3 :
+    print('Os segmentos acima PODEM FORMAR triângulo ISÓSCELES!')
+elif r1 != r2 and r1 and r3 and r2 != r3 :
+    print('Os segmentos acima PODEM FORMAR triângulo ESCALENO!')
