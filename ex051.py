38183d9b8a81f1a3a57bbfe8ea0e33aaadfcc4d7
Victor Maciel <victormaciel.dev@gmail.com>
2024-10-17 22:39:15 -0300

    Create ex051.py

---
+++ ex051.py
@@ -0,0 +1,12 @@
+print('=' * 30)
+print('     10 TERMOS DE UMA PA    ')
+print('=' * 30)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+soma = 0
+
+print(primeiro)
+for c in range(1, 10):
+    soma = soma + razao
+    print(soma)
+print('ACABOU')
